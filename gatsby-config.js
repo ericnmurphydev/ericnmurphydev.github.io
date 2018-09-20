e223989d37d55fa406ab3fa6696ea4af882e4ad1
Eric Murphy <ericnmurphy@gmail.com>
2018-09-20 17:04:22 +0700

    Set up for Github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Gatsby Default Starter',
     desc: 'A new blog',
   },
+  pathPrefix: '/gatsby',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
